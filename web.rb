8bea6f567cd2443e2be5d1a03dbc4bda55445835
mklepp <me@mklepp.com>
2014-11-25 01:45:29 +0100

    S3 File upload

--- web.rb
+++ web.rb
@@ -1,10 +1,32 @@
 require 'sinatra'
+require 'aws/s3'
 
 set :port, ENV["PORT"] || 5000
 
 get '/' do
-  whom = ENV["POWERED_BY"] || "Deis!"
-  container = `hostname`.strip || "unknown"
-  "Powered by " + whom + "\nRunning on container ID " + container + "\n"
+  @whom = ENV["POWERED_BY"] || "Deis!"
+  @container = `hostname`.strip || "unknown"
+  erb :index :locals => {  whom: @whom, container: @container}
+end
 
+post '/upload' do
+  upload(params[:content]['file'][:filename], params[:content]['file'][:tempfile])
+  redirect '/'
+end
+
+helpers do
+  def upload(filename, file)
+    bucket = 'bucket_name'
+    AWS::S3::Base.establish_connection!(
+    server:             ENV['S3_HOST'],
+    access_key_id:      ENV['S3_ACCESS_KEY_ID'],
+    secret_access_key:  ENV['S3_SECRET_ACCESS_KEY']
+    )
+    AWS::S3::S3Object.store(
+    filename,
+    open(file.path),
+    bucket
+    )
+    return filename
+  end
 end
